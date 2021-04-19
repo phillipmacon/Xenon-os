c4a50fcf90b8de8c0404853ea498845827fe1a2f
Lucas Cohaereo <lucasteal@protonmail.com>
2021-04-19 21:28:50 +0200

    mem: Simplify bitmap

--- kernel/mem/pmm.cpp
+++ kernel/mem/pmm.cpp
@@ -2,6 +2,34 @@
 #include <print.h>
 #include "mem.h"
 
+namespace util {
+    void Bitmap::set(size_t index) {
+        bits[index / 64] |= (1 << (index % 64));
+    }
+
+    void Bitmap::unset(size_t index) {
+        bits[index / 64] &= ~(1 << (index % 64));
+    }
+
+    u8 Bitmap::get(size_t index) {
+        return bits[index / 64] & (1 << (index % 64));
+    }
+
+    u64 Bitmap::getNextZero() {
+        for(size_t i = 0; i < data_size; i++) {
+            // Skip block if the entire block is taken
+            if(bits[i] == ~0ULL) continue;
+
+            // Iterate through block
+            for(size_t ib = 0; ib < 64; ib++) {
+                if(get(data_size * 64 + ib) == 0)
+                    return data_size * 64 + ib;
+            }
+        }
+    }
+}
+
+
 extern addr _start;
 extern addr _end;
 
