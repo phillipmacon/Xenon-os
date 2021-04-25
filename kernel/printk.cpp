cbfc12852aaf98b195348999be1547d6667e08a7
Lucas Cohaereo <lucasteal@protonmail.com>
2021-04-25 14:50:38 +0200

    printk: Implement base 10 format specifier

--- kernel/printk.cpp
+++ kernel/printk.cpp
@@ -1,6 +1,64 @@
 #include <print.h>
 #include <starg.h>
 
+// TODO: Move string functions to their own file
+size_t strlen(char* s) {
+    size_t res{};
+    while(*s) {
+        res++;
+        s++;
+    }
+
+    return res;
+}
+
+void reverse(char* s) {
+    u32 i, j;
+
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+        char c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+char* itoa(i64 num, char* str, u32 base) {
+    i64 i = 0;
+    bool neg = false;
+
+    if (num == 0) {
+        str[i++] = '0';
+        str[i] = '\0';
+        return str;
+    }
+
+    if (num < 0 && base == 10) {
+        neg = true;
+        num = -num;
+    }
+  
+    while (num != 0) {
+        i64 rem = num % base;
+        str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
+        num = num / base;
+    }
+
+    if (neg) 
+        str[i++] = '-';
+  
+    str[i] = '\0';
+    reverse(str);
+
+    return str;
+} 
+
+char toLower(char c) {
+    if(0x41 >= c && c <= 0x5a)
+        c += 0x20;
+    
+    return c;
+}
+
 [[gnu::noinline]] void puts(const char* s) {
     while(*s) {
         putc(*s);
@@ -26,11 +84,11 @@
     puts(&buf[start_index]);
 }
 
-char toLower(char c) {
-    if(0x41 >= c && c <= 0x5a)
-        c += 0x20;
-    
-    return c;
+[[gnu::noinline]] void puti(i64 x, u32 base=10) {
+    char buf[32];
+
+    itoa(x, buf, base);
+    puts(buf);
 }
 
 void printklImpl(LogLevel level, const char* fmt, va_list args) {
@@ -53,6 +111,13 @@ void printklImpl(LogLevel level, const char* fmt, va_list args) {
                 case 'x': {
                     u64 value = va_arg(args, u64);
                     putx(value);
+                    in_fmt = false;
+                    break;
+                }
+                case 'i':
+                case 'd': {
+                    i64 value = va_arg(args, i64);
+                    puti(value);
                     in_fmt = false;
                     break;
                 }
