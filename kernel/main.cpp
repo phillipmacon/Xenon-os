134f214239629f1a45aa7cc5b2cc41473a812400
Luca <cohaereo@protonmail.com>
2021-11-30 09:02:50 +0100

    acpi: Remove ACPI module for the time being

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,10 +1,10 @@
-#include <acpi.h>
 #include <array.h>
 #include <cpu/cpuinfo.h>
 #include <cpu/interrupts.h>
 #include <cpu/gdt.h>
 #include <cpu/paging.h>
 #include <fb.h>
+#include <mem.h>
 #include <print.h>
 #include <stivale/stivale2.h>
 #include <types.h>
@@ -73,12 +73,6 @@ extern "C" void kmain(stivale2_struct* boot_info)
             }
             break;
         }
-        case STIVALE2_STRUCT_TAG_RSDP_ID: {
-            printk("Found RSDP\n");
-            stivale2_struct_tag_rsdp* rsdptag = reinterpret_cast<stivale2_struct_tag_rsdp*>(tag);
-            uefi::ACPI::instance().init(rsdptag->rsdp);
-            break;
-        }
         }
 
         tag = (stivale2_tag*)tag->next;
@@ -87,36 +81,15 @@ extern "C" void kmain(stivale2_struct* boot_info)
     util::memset(fb, 0x00, fbWidth * fbHeight * 4);
     video::fb::init(fb, fbWidth, fbHeight);
 
-    void* bgrt_ptr = uefi::ACPI::instance().getTable("BGRT");
-    if(!bgrt_ptr) {
-        printk("BGRT table not found\n");
+    u32 midX = fbWidth / 2, midY = fbHeight / 2;
+    u32 midLogo = (midY - 190) * fbWidth + (midX - 190);
 
-        u32 midX = fbWidth / 2, midY = fbHeight / 2;
-        u32 midLogo = (midY - 190) * fbWidth + (midX - 190);
-
-        for(u32 y = 0; y < 380; y++) {
-            for(u32 x = 0; x < 380; x++) {
-                u8 p = _xenonLogo[y * 380 + x];
-                fb[midLogo + y * fbWidth + x] = (p << 16) | (p << 8) | p;
-            }
-        }
-    } else {
-        printk("BGRT table found!\n");
-        uefi::BGRT* bgrt = reinterpret_cast<uefi::BGRT*>(bgrt_ptr);
-        u8* img_ptr = reinterpret_cast<u8*>(bgrt->image_address + mem::constants::kernelBase);
-        BitmapHeader* bh = reinterpret_cast<BitmapHeader*>(img_ptr);
-        BitmapInfoHeader* bih = reinterpret_cast<BitmapInfoHeader*>(img_ptr + sizeof(BitmapHeader));
-        RGB24* img_data = reinterpret_cast<RGB24*>(img_ptr + bh->offset);
-
-        for(u32 y = 0; y < bih->image_height; y++) {
-            for(u32 x = 0; x < bih->image_width; x++) {
-                RGB24 pixel = img_data[(bih->image_height - y - 1) * bih->image_width + x];
-                fb[(bgrt->image_offset_y + y) * fbWidth + x + bgrt->image_offset_x] = pixel.r << 16 | pixel.g << 8 | pixel.b;
-            }
+    for(u32 y = 0; y < 380; y++) {
+        for(u32 x = 0; x < 380; x++) {
+            u8 p = _xenonLogo[y * 380 + x];
+            fb[midLogo + y * fbWidth + x] = (p << 16) | (p << 8) | p;
         }
     }
-
-    uefi::ACPI::instance().printTables();
 
     printk("%iMB memory detected, %iMB available\n", util::bytesToMb(memsize), util::bytesToMb(memsize_usable));
     printk("%iKB for bitmap\n", util::bytesToKb(memsize / 4096 / 8));
